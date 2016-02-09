9af4bbc40fa5de4f397e29aa7413211fced1cf2b
Geetha Ratnam <geetharatnam1@gmail.com>
2016-02-08 17:43:32 -0800

    added all files

---
+++ server/server.js
@@ -0,0 +1,15 @@
+var express = require('express');
+var app = express();
+app.use(express.static('client'));
+app.use(express.static('bower_components'));
+// app.get('/', function (req, res) {
+//   res.send('Hello World!');
+// });
+
+var prodPort = process.env.PORT || 8080
+var server = app.listen(prodPort, function () {
+  var host = server.address().address;
+  var port = server.address().port;
+
+  console.log('Example app listening at http://%s:%s', host, port);
+});
